dbeb631572c5d36699005b22870dddb1fb331ae1
Daksh Goel <dakshgoel2006@gmail.com>
2025-12-30 17:54:49 +0530

    Add Arcjet config with bot detection and token bucket rate limiting

---
+++ backend/src/config/arcjet.ts
@@ -0,0 +1,26 @@
+import arcjet, {tokenBucket, shield, detectBot} from "@arcjet/node";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const aj = arcjet({
+    key: process.env.ARCJET_KEY!,
+    characteristics: ["ip.src"],
+    rules: [
+        shield({mode: "LIVE"}),
+
+        detectBot({
+            mode: "LIVE",
+            allow: [
+                "CATEGORY:SEARCH_ENGINE"
+            ]
+        }),
+
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 10,
+            interval: 60,
+            capacity: 10,
+        }),
+    ],
+}) 
