bf83d67e77281d8ec720e662e23742662d745d00
Daksh Goel <dakshgoel2006@gmail.com>
2025-12-30 20:25:02 +0530

    Wrap app with ClerkProvider and SafeArea context

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,21 @@
+import 'react-native-reanimated';
 import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { StatusBar } from "expo-status-bar";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import "../global.css"
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <StatusBar style="light" />
+        <Stack screenOptions={{headerShown: false, contentStyle: {backgroundColor: '#050505'}}}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaProvider>
+    </ClerkProvider>
+  );
 }
