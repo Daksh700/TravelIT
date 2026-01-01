fb6a484a0bef33e40f4952f624a6b83cde68efff
Daksh Goel <dakshgoel2006@gmail.com>
2026-01-01 17:39:10 +0530

    make user profile update schema fields optional

--- backend/src/validators/userValidator.ts
+++ backend/src/validators/userValidator.ts
@@ -1,8 +1,8 @@
 import {z} from "zod"
 
 export const updateUserSchema = z.object({
-    firstName: z.string().min(1, "First Name is required"),
-    lastName: z.string().optional(),
-    username: z.string().optional(),
+    firstName: z.string().min(1).optional(),
+    lastName: z.string().min(1).optional(),
+    username: z.string().min(1).optional(),
     avatar: z.url("Avatar must be a valid URL").optional()
 })
