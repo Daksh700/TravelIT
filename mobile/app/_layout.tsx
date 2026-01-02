14d3fec707644af969b6ade9d32d70efd7551f49
Daksh Goel <dakshgoel2006@gmail.com>
2026-01-02 15:23:13 +0530

    add QueryClientProvider to root layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,17 +5,22 @@ import { StatusBar } from "expo-status-bar";
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import "../global.css"
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaProvider>
-        <StatusBar style="light" />
-        <Stack screenOptions={{headerShown: false, contentStyle: {backgroundColor: '#050505'}}}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        </Stack>
-      </SafeAreaProvider>
-    </ClerkProvider>
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider tokenCache={tokenCache}>
+        <SafeAreaProvider>
+          <StatusBar style="light" />
+          <Stack screenOptions={{headerShown: false, contentStyle: {backgroundColor: '#050505'}}}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack>
+        </SafeAreaProvider>
+      </ClerkProvider>
+    </QueryClientProvider>
   );
 }
