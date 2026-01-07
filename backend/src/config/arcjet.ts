32fe8b47a54c8bf5b03ea3c682f0b8670c003600
Daksh Goel <dakshgoel2006@gmail.com>
2026-01-07 18:17:44 +0530

    update arcjet config

--- backend/src/config/arcjet.ts
+++ backend/src/config/arcjet.ts
@@ -1,26 +1,28 @@
-import arcjet, {tokenBucket, shield, detectBot} from "@arcjet/node";
 import dotenv from "dotenv"
 
 dotenv.config();
 
-export const aj = arcjet({
-    key: process.env.ARCJET_KEY!,
-    characteristics: ["ip.src"],
-    rules: [
-        shield({mode: "LIVE"}),
+export const aj = (async () => {
+    const { default: arcjet, tokenBucket, shield, detectBot } = await import("@arcjet/node");
+    return arcjet({
+        key: process.env.ARCJET_KEY!,
+        characteristics: ["ip.src"],
+        rules: [
+            shield({mode: "LIVE"}),
 
-        detectBot({
-            mode: "LIVE",
-            allow: [
-                "CATEGORY:SEARCH_ENGINE"
-            ]
-        }),
+            detectBot({
+                mode: "LIVE",
+                allow: [
+                    "CATEGORY:SEARCH_ENGINE"
+                ]
+            }),
 
-        tokenBucket({
-            mode: "LIVE",
-            refillRate: 10,
-            interval: 60,
-            capacity: 10,
-        }),
-    ],
-}) 
+            tokenBucket({
+                mode: "LIVE",
+                refillRate: 10,
+                interval: 60,
+                capacity: 10,
+            }),
+        ],
+    });
+})();
