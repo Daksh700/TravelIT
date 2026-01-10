e95fabbe16a0a7e95e1091b0fb7710b0bf4f65f1
Daksh Goel <dakshgoel2006@gmail.com>
2026-01-10 21:04:29 +0530

    feat: add exploreLocation API call with token auth and error handling

---
+++ mobile/services/explore.ts
@@ -0,0 +1,32 @@
+export const exploreLocation = async(
+    token: string,
+    query: string
+) => {
+    try {
+        console.log("Connecting to Backend");
+
+        const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/itinerary/explore`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                query
+            })
+        })
+
+        const data = await response.json();
+
+        if(!response.ok) {
+            throw new Error(data.message || "Something went wrong");
+        }
+
+        console.log("Data Received from Backend");
+
+        return data.data;
+    } catch (error) {
+        console.error("API Error: ", error);
+        return null;
+    }
+}
