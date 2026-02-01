0a871856404102aae2e18a4e26778aa08567ad53
Daksh Goel <dakshgoel2006@gmail.com>
2026-02-01 16:16:54 +0530

    refactor: increase the allowed size limit of incoming json to 1mb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Body parsing middleware first
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
 
 // Then authentication middleware
 app.use(clerkMiddleware());
