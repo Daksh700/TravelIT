327121e8e04048fe71cf8b595be4e0c442f35439
Daksh Goel <dakshgoel2006@gmail.com>
2026-02-01 16:45:44 +0530

    refactor: exclude location from verified data and and prevent overwrite of location string

--- backend/src/controllers/aiController.ts
+++ backend/src/controllers/aiController.ts
@@ -160,7 +160,8 @@ ${interestPrompt}
 
       for(const act of day.activities) {
         const verified = await verifyPlace(act.activity, `${act.location} ${destination}`);
-        verifiedActs.push({...act, ...verified});
+        const {location, ...verifiedData} = verified; 
+        verifiedActs.push({...act, ...verifiedData}); 
       }
 
       verifiedDays.push({...day, activities: verifiedActs});
