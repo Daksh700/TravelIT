022136167a19159476f90b357a72786a5f8f360c
Daksh Goel <dakshgoel2006@gmail.com>
2026-03-03 17:01:39 +0530

    chore: register tinder screen in trips stack

--- mobile/app/(tabs)/trips/_layout.tsx
+++ mobile/app/(tabs)/trips/_layout.tsx
@@ -5,6 +5,7 @@ export default function TripsStackLayout() {
         <Stack screenOptions={{headerShown: false}}>
             <Stack.Screen name="index"/>
             <Stack.Screen name="view" />
+            <Stack.Screen name="tinder"/>
         </Stack>
     )
 }
